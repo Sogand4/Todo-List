e25b3dcd9e14477e1872a8c4fdc2d98e0bacd25f
Sogand <sogand.h.salimi@gmail.com>
2022-11-14 19:28:30 -0800

    Organization and function documentations

--- index.js
+++ index.js
@@ -10,16 +10,11 @@ require('dotenv').config();
 let PORT = process.env.PORT;
 app.set('port', process.env.PORT);
 
-app.listen(PORT, function () {
-  console.log('Server started on port ' + PORT);
-});
-
 let task = [];
 let complete = [];
-app.post('/addtask', function (req, res) {
-  let newTask = req.body.newtask;
-  task.push(newTask);
-  res.redirect("/");
+
+app.listen(PORT, function () {
+  console.log("Server started on port " + PORT);
 });
 
 app.get("/", function(req, res) {    
@@ -30,13 +25,24 @@ app.get("*", function(req, res) {
   res.send("<h1>Invalid Page</h1>");
 });
 
-app.post("/completetask", function(req, res) {
+/**
+ * Adds an item to tasks
+ */
+app.post('/addtask', function (req, res) {
+  let newTask = req.body.newtask;
+  task.push(newTask);
+  res.redirect("/");
+});
+
+/**
+ * Removes an item from tasks
+ */
+ app.post("/removetask", function(req, res) {
   let deleteTask = req.body.check;
 
   for (let i = 0; i < task.length; i++) {
     if (task[i] == deleteTask) {
       task.splice(i, 1);
-      complete.push(deleteTask);
       break;
     }
   }
@@ -44,12 +50,16 @@ app.post("/completetask", function(req, res) {
   res.redirect("/");
 });
 
-app.post("/removetask", function(req, res) {
+/**
+ * Removes an item from tasks and adds it to completed tasks
+ */
+app.post("/completetask", function(req, res) {
   let deleteTask = req.body.check;
 
   for (let i = 0; i < task.length; i++) {
     if (task[i] == deleteTask) {
       task.splice(i, 1);
+      complete.push(deleteTask);
       break;
     }
   }
