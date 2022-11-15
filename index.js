46b6ed35060b61e641ae245414606c00d9055c9e
Sogand <sogand.h.salimi@gmail.com>
2022-11-14 18:34:32 -0800

    Added some styling, nodemon, and .env file

--- index.js
+++ index.js
@@ -4,35 +4,55 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
-app.listen(8080, function () {
-  console.log('Server started on port 8080')
+require('dotenv').config();
+let PORT = process.env.PORT;
+app.set('port', process.env.PORT);
+
+app.listen(PORT, function () {
+  console.log('Server started on port ' + PORT);
 });
 
 let task = [];
+let complete = [];
 app.post('/addtask', function (req, res) {
-    let newTask = req.body.newtask;
-    task.push(newTask);
-    res.redirect("/");
+  let newTask = req.body.newtask;
+  task.push(newTask);
+  res.redirect("/");
 });
 
 app.get("/", function(req, res) {    
-  res.render("index", { task: task});
+  res.render("index", { task: task, complete: complete});
 });
 
 app.get("*", function(req, res) {
   res.send("<h1>Invalid Page</h1>");
 });
 
-app.post("/removetask", function(req, res) {
-    let deleteTask = req.body.check;
+app.post("/completetask", function(req, res) {
+  let deleteTask = req.body.check;
 
-    for (let i = 0; i < task.length; i++) {
-      if (task[i] == deleteTask) {
-        task.splice(i, 1);
-        break;
-      }
+  for (let i = 0; i < task.length; i++) {
+    if (task[i] == deleteTask) {
+      task.splice(i, 1);
+      complete.push(deleteTask);
+      break;
     }
+  }
 
-   res.redirect("/");
+  res.redirect("/");
+});
+
+app.post("/removetask", function(req, res) {
+  let deleteTask = req.body.check;
+
+  for (let i = 0; i < task.length; i++) {
+    if (task[i] == deleteTask) {
+      task.splice(i, 1);
+      break;
+    }
+  }
+
+  res.redirect("/");
 });
